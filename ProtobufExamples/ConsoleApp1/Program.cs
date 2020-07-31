394617712516876900a8c1a6bdd0035405c75e98
Fred <my@email.com>
2020-07-31 09:13:49 +0200

    Testing out protobuf

--- ProtobufExamples/ConsoleApp1/Program.cs
+++ ProtobufExamples/ConsoleApp1/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Google.Protobuf.Examples.Person;
+using Google.Protobuf;
+using System.IO;
 
 namespace ConsoleApp1
 {
@@ -7,6 +10,38 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            CreateFile();
+
+            //Read from file.
+            Person female;
+            using (var input = File.OpenRead("lena.dat"))
+            {
+                female = Person.Parser.ParseFrom(input);
+            }
+
+            Console.WriteLine("Name: " + female.Name);
+            Console.WriteLine("Age: " + female.Age);
+
+
+            Console.ReadLine();
+        }
+
+        static void CreateFile()
+        {
+            Person lena = new Person { Name = "Lena", Age = 32 };
+            try
+            {
+                using (var output = File.Create("lena.dat"))
+                {
+                    lena.WriteTo(output);
+                }
+                Console.WriteLine("File created!");
+            }
+            catch
+            {
+                Console.WriteLine("Something went wrong with file creation.");
+            }
+            //"C:\\repos\\CmdAppdotnetProtobufExamples\\ProtobufExamples\\ConsoleApp1\\bin\\Debug\\netcoreapp3.1\\john.dat"
         }
     }
 }
